4b62a8c21725de4742f4fd4a590d58cc80ff1cfe
tijanajeremic <tj20200192@student.fon.bg.ac.rs>
2024-12-04 18:14:32 +0100

    CRUD OPERACIJE GARDEROBER

--- domaci1/routes/api.php
+++ domaci1/routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\GarderoberController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -22,5 +24,14 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::get('/me', [AuthController::class, 'me']);
+
+
+
+    Route::get('/garderoberi', [GarderoberController::class, 'index']);
+    Route::get('/garderoberi/{id}', [GarderoberController::class, 'show']);
+    Route::post('/garderoberi', [GarderoberController::class, 'store']);
+    Route::put('/garderoberi/{id}', [GarderoberController::class, 'update']);
+    Route::delete('/garderoberi/{id}', [GarderoberController::class, 'destroy']);
 });
 
+ 
